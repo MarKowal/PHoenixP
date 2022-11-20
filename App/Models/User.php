035fe84b491ae897f865672446afd3301b25c032
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-11-20 19:30:04 +0100

    Validation of user's register data

--- App/Models/User.php
+++ App/Models/User.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use PDO;
 
 class User extends \Core\Model{
+
+    public $errors = [];
+
     public function __construct($data){
         foreach($data as $key => $value){
             $this->$key = $value;
@@ -12,21 +15,52 @@ class User extends \Core\Model{
     }
 
     public function save(){
-        $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+        $this->validate();
 
-        $sql = 'INSERT INTO users (name, email, password_hash) VALUES (:name, :email, :password_hash)';
+        if (empty($this->errors)){
 
-        $db = static::getDB();
-        $stmt = $db->prepare($sql);
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
 
-        $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-        $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+            $sql = 'INSERT INTO users (name, email, password_hash) VALUES (:name, :email, :password_hash)';
 
-        $stmt->execute();
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
 
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        } 
+        return false;
     }
     
+    public function validate(){
+
+        if ($this->name == ''){
+            $this->errors[] = 'Name is required';
+        }
+
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false){
+            $this->errors[] = 'Invalid email';
+        }
+        
+        if ($this->password != $this->passwordConfirm){
+            $this->errors[] = 'Password must match confirmation';
+        }
+
+        if (strlen($this->password)<6){
+            $this->errors[] = 'Please enter at least 6 characters for the password';
+        }
+
+        if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs at least one letter';
+        }
+ 
+        if (preg_match('/.*\d+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs at least one number';
+        }
+    }
 }
 
 
