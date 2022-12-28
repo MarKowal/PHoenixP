6a9e37cced9096c26c820d58355e971a035e5a0c
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2022-12-28 20:21:51 +0100

    Add the remember_me checkbox

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -18,6 +18,8 @@ class Login extends \Core\Controller{
         //var_dump($user);
 
         $user = User::authenticate($_POST['email'], $_POST['password']);
+        $remember_me = isset($_POST['remember_me']);
+        
 
         if($user){
             //header('Location: http://'.$_SERVER['HTTP_HOST'].'/', true, 303);
@@ -32,7 +34,10 @@ class Login extends \Core\Controller{
         } else{
             Flash::addMessage('Login unsuccessful, please try again', Flash::WARNING);
 
-            View::renderTemplate('Login/new.html', ['email' => $_POST['email'], ]);
+            View::renderTemplate('Login/new.html', [
+                'email' => $_POST['email'], 
+                'remember_me' => $remember_me
+            ]);
         }
     }
 
