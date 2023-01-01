92ac9af40104a7a3d2316957f531dfc9e30c7e57
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-01-01 18:03:45 +0100

    Password_reset_hash saved in database

---
+++ App/Controllers/Password.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use \Core\View;
+
+
+class Password extends \Core\Controller{
+    public function forgotAction(){
+        View::renderTemplate('Password/forgot.html');
+    
+    }
+
+    public function requestResetAction(){
+        User::sendPasswordReset($_POST['email']);
+        View::renderTemplate('Password/reset_requested.html');
+    }
+
+}
+
+?>
