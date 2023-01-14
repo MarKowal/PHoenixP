e453e189ec9eb3af4867b264db55d5ecec204909
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-01-14 21:59:26 +0100

    Display profile data

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Auth;
+
+//class Profile extends \Core\Controller{
+
+class Profile extends Authenticated{
+
+
+    public function showAction(){
+        View::renderTemplate('Profile/show.html',[ 
+            'user' => Auth::getUser()
+        ]);
+    }
+    
+}
+
+
+
+?>
